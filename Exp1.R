e633c87ee3cdcfaaaf58c0b0d5cd7c8e292a409c
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-02 13:34:17 +0530

    Experiment1

---
+++ Exp1.R
@@ -0,0 +1,44 @@
+# ** UNIT - 1 **
+# ** Overview of R **
+
+# EXPERIMENTS :-
+
+# First :-
+num1 <- 100
+num2 <- 50
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(sqrt(num1))
+print(log10(num1))
+print(exp(num1))
+print(num1*num1)
+
+# Second :-
+X <-100
+Y <- 20
+X <- X / Y
+print(X)
+
+# Third :-
+x <- 10
+y <- 14
+sequence = seq(x,y,by=0.3)
+print(sequence)
+
+# Fourth :-
+sequence <- rev(sequence)
+print(sequence)
+
+# Fifth :-
+vec <- c(1:20)
+vec
+print(length(vec))
+
+# Sixth :-
+first <- vec[1]
+last <- vec[length(vec)]
+print(first)
+print(last)
+
